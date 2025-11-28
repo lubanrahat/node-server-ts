566978f1a7ee1e6022ee90e119388f863bc08ba3
Luban Rahat <lubanrahat.dev@gmail.com>
2025-11-28 13:05:21 +0600

    create http server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
-import http, { IncomingMessage, Server, ServerResponse } from "http"
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+import ENV from "./config/env";
 
-const server: Server = http.createServer((req: IncomingMessage,res: ServerResponse) => {
-    if(req.url === "/" && req.method === "GET") {
-        
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    if (req.url === "/" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Hello from node js with typescript",
+          path: req.url,
+        })
+      );
     }
-})
+  }
+);
+
+server.listen(ENV.PORT, () => {
+  console.log(`Server is rinngin port ${ENV.PORT}`);
+});
